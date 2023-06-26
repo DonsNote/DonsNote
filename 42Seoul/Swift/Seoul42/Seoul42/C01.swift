7e50c465edc1b9d4e6461c3397d181902a7bab84
Don <neveradio@gmail.com>
2023-06-26 14:03:58 +0900

    go
    go

---
+++ 42Seoul/Swift/Seoul42/Seoul42/C01.swift
@@ -0,0 +1,19 @@
+//
+//  C01.swift
+//  Seoul42
+//
+//  Created by Kimdohyun on 2023/06/24.
+//
+
+import Foundation
+
+var a = 1
+var b = 2
+var c
+
+func sum (a,b) -> Int
+{
+    a + b = c
+    
+    return c
+}
---
+++ 42Seoul/Swift/Seoul42/Seoul42/Seoul42App.swift
@@ -0,0 +1,17 @@
+//
+//  Seoul42App.swift
+//  Seoul42
+//
+//  Created by Kimdohyun on 2023/06/24.
+//
+
+import SwiftUI
+
+@main
+struct Seoul42App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
